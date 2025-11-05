4e330db36a3b2ec10d465498bec561dbb53ef5c9
Kshitij Verma <kshitijverma577@gmail.com>
2025-11-05 23:05:39 +0530

    make some changes in gcd question

--- 4-function/tut4_hcf.cpp
+++ 4-function/tut4_hcf.cpp
@@ -1,20 +1,51 @@
-
-
 // find gcd of two numbers
 
 #include<iostream>
 using namespace std;
 
-int gcd(int a, int b){
-    int hcf=1;
-    for(int i=2;i<=min(a,b);i++){
-        if(a%i==0 && b%i==0){
-            hcf=i;
-        }
-    }
-    return hcf;
-}
+// int gcd(int a, int b){
+//     int hcf=1;
+//     for(int i=2;i<=min(a,b);i++){
+//         if(a%i==0 && b%i==0){
+//             hcf=i;
+//         }
+//     }
+//     return hcf;
+// }
 
+
+// int gcd(int a, int b){
+//     int hcf=1;
+//     for(int i=min(a,b);i>=1;i--){
+//         if(a%i==0 && b%i==0){
+//             hcf=i;
+//             break;
+//         }
+//     }
+//     return hcf;
+// }
+
+int gcd_euclids(int a, int b){
+    
+    int divisor=min(a,b);
+    int dividend=max(a,b);
+   
+    if(dividend==0 || divisor==0){
+        return max(a,b);
+    }
+
+    while(divisor!=0){
+        
+        int remainder=dividend%divisor;
+       
+            dividend=divisor;
+            divisor=remainder;
+        
+
+    }
+    return dividend;
+    
+}
 
 int main(){
     int a;
@@ -25,7 +56,7 @@ int main(){
     cout<<"ENter second number:";
     cin>>b;
 
-    cout<<gcd(a,b);
+    cout<<gcd_euclids(a,b);
 
     
 }
