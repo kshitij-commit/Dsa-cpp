96c02a1356fe6ae52f2e4c827619d2a5f34a7faa
Kshitij Verma <kshitijverma577@gmail.com>
2026-01-04 21:17:01 +0530

    completed iterators

--- 8-Vector/tut4_iterators.cpp
+++ 8-Vector/tut4_iterators.cpp
@@ -16,14 +16,15 @@ using namespace std;
 //     }
 // }
 
-// int main()
-// {
-//     vector<int> v = {1, 2, 3, 4, 5};
-//     for(int i=0;i<v.size();i++){
-//         v[i]+=i;
-//     }
-//     for (int i : v)
-//     {
-//         cout << i << " ";
-//     }
-// }
+int main()
+{
+    vector<int> v = {1, 2, 3, 4, 5};
+    for(int i=0;i<v.size();i++){
+        v[i]+=i;
+    }
+   
+    for (int i : v)
+    {
+        cout<<i<<" ";        
+    }
+}
