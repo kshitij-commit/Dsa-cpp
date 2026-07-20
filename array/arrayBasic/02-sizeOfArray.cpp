f1a0194ec3ec09771554ee64990d48cbc18e789b
Kshitij Verma <kshitijverma577@gmail.com>
2026-07-20 23:04:12 +0530

    feat: add finding size of array

---
+++ array/arrayBasic/02-sizeOfArray.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+                                 // You can take any of its element.
+    int size = sizeof(arr)/sizeof(arr[2]);  
+    cout<< size;
+}
