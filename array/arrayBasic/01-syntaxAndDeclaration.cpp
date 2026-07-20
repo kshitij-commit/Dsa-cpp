dd2b7061edd7a4c664319126c29c2b2047af43a7
Kshitij Verma <kshitijverma577@gmail.com>
2026-07-20 23:03:29 +0530

    feat: add basic syntax and declaration of array

---
+++ array/arrayBasic/01-syntaxAndDeclaration.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[3] = {1,2,3};
+    cout<<arr[0];
+
+    int arr1[] = {1,2,3,4};
+
+    // int arr2[];   //error
+    cout<<endl;
+    for (int i=0;i<4;i++){
+        cout<<arr1[i]<<" ";
+    }
+
+
+}
