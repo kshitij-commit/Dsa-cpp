743b6e490d96ff679be3557df8753130bf629e57
Kshitij Verma <kshitijverma577@gmail.com>
2026-07-21 22:41:10 +0530

    feat: add basics of pointers

---
+++ array/pointers/01-pointerAndArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int* ptr = arr;
+    // int* ptr = &arr[0];
+
+    cout<<ptr<<endl;
+    cout<<&arr[0]<<endl;
+
+    cout<<ptr[1]<<endl;
+
+    for(int i=0; i<5;i++){
+        cout<<ptr[i]<<" ";
+    }
+
+
+
+
+
+    return 0;
+
+
+}
