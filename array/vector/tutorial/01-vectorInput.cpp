9a78dbdfe731d8c4f8dacd63d870c4c6741dd635
Kshitij Verma <kshitijverma577@gmail.com>
2026-07-25 01:46:39 +0530

    feat: add taking  vector input

---
+++ array/vector/tutorial/01-vectorInput.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int main(){
+    vector<int> v;
+
+    for(int i=0; i<5; i++){
+        int x;
+        cin>>x;
+        v.push_back(x);
+    }
+
+    for(int i = 0; i < 5;i++){
+        cout<<v[i]<<" ";
+    }
+}
