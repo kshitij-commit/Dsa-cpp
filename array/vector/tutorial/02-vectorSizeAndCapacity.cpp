365eb554152c1d8204284e3ed8ec94797aa82221
Kshitij Verma <kshitijverma577@gmail.com>
2026-07-25 01:48:33 +0530

    feat: add vector size and vector capacity demonstration

---
+++ array/vector/tutorial/02-vectorSizeAndCapacity.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+#include<vector>
+
+int main(){
+    vector<int> v;
+
+    v.push_back(3);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(7);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(3);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(4);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(5);
+    cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(6);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(6);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    v.push_back(6);
+     cout<<"size is: "<<v.size()<<endl;
+    cout<<"capacity is: "<<v.capacity()<<endl;
+    
+}
