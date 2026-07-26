a8a878c54a3f11fd7913529d7a7e9f603a88ef7c
Kshitij Verma <kshitijverma577@gmail.com>
2026-07-26 21:47:33 +0530

    feat: add pass vector by reference

---
+++ array/vector/tutorial/05-vectorPassByReference.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+
+
+void change(vector<int>& a){
+    a[0] = 100;
+
+}
+
+int main(){
+    vector<int> v;
+
+    v.push_back(5);
+    v.push_back(10);
+    v.push_back(15);
+    v.push_back(20);
+    
+    // cout<<v.size();
+
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    change(v);
+    for(int i=0;i<v.size();i++){
+        cout<<v[i]<<" ";
+    }
+    cout<<endl;
+
+    cout<<"vector element  changed";
+    
+}
