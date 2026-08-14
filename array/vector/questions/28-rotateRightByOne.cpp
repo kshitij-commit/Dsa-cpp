50b8b62065ba65078e475014b3cbc49577b83b65
Kshitij Verma <kshitijverma577@gmail.com>
2026-08-14 23:17:09 +0530

    feat: add rotate array by one

---
+++ array/vector/questions/28-rotateRightByOne.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void rotate(int arr[],int n)
+{
+    int last=arr[n-1];
+
+    for(int i=n-1;i>0;i--)
+        arr[i]=arr[i-1];
+
+    arr[0]=last;
+}
+
+int main()
+{
+    int arr[]={1,2,3,4,5};
+
+    rotate(arr,5);
+
+    for(int x:arr)
+        cout<<x<<" ";
+}
