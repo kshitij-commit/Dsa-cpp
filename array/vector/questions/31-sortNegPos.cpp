8807989f394482b7507e31b2cbce3f43ca8dfead
Kshitij Verma <kshitijverma577@gmail.com>
2026-08-15 23:28:44 +0530

    feat: add sort negative to beginning while positive to end.

---
+++ array/vector/questions/31-sortNegPos.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// move all negative to beginning and positive to end.
+
+int main()
+{
+    vector<int> v;
+    int size;
+    cout << "Enter size of vector: ";
+    cin >> size;
+
+    if (size > 0)
+    {
+
+        cout << "Enter vector elements: ";
+
+        for (int i = 0; i < size; i++)
+        {
+            int x;
+            cin >> x;
+            v.push_back(x);
+        }
+        if (size > 1)
+        {
+            int i = 0;
+            int j = size - 1;
+            while (i < j)
+            {
+                if (v[i] < 0)
+                    i++;
+                else if (v[j] >= 0)
+                    j--;
+                else 
+                {
+                    int temp = v[i];
+                    v[i] = v[j];
+                    v[j] = temp;
+                    j--;
+                    i++;
+                }
+            }
+        }
+
+        for (int i = 0; i < size; i++)
+        {
+            cout << v[i] << " ";
+        }
+    }
+
+    return 0;
+}
