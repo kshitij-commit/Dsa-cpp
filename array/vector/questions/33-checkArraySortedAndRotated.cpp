4d61d565d0b164025f0e8a761f4d909f7f5af125
Kshitij Verma <kshitijverma577@gmail.com>
2026-08-23 23:00:11 +0530

    feat: add array is sorted or rotated

--- array/vector/questions/33-checkArraySortedAndRotated.cpp
+++ array/vector/questions/33-checkArraySortedAndRotated.cpp
@@ -1,38 +1,40 @@
-#include <iostream>
-#include <vector>
+// #33 — Check if Array Is Sorted and Rotated
+
+// Question:
+// Given an integer array nums, return true if the array was originally sorted 
+// in non-decreasing/ascending order and then rotated some number of positions. Otherwise, return false.
+#include<iostream>
+#include<vector>
+
 using namespace std;
 
-bool checkSortedRotated(vector<int>& v) {
-    int count = 0;
-    int n = v.size();
 
-    for (int i = 0; i < n; i++) {
-        // Agar agla element pichle se chota hai, toh ek dip mila
-        if (v[i] > v[(i + 1) % n]) {
+
+bool checkSortedRotated(vector<int> v){
+
+    int count = 0;
+    
+    for(int i=0;i<v.size();i++){
+        if(v[i]>v[(i+1)%v.size()])
             count++;
-        }
     }
 
-    // Sorted and rotated vector mein zyada se zyada 1 hi dip ho sakta hai
-    return (count <= 1);
+    return count <=1;
+
 }
 
-int main() {
-    // Test Case 1: Sorted aur Rotated hai (True aana chahiye)
-    vector<int> v1 = {3, 4, 5, 1, 2}; 
-    if (checkSortedRotated(v1)) {
-        cout << "v1: True (Sorted & Rotated)" << endl;
-    } else {
-        cout << "v1: False" << endl;
+
+int main(){
+    int size;
+    cout<<"Enter vector size: ";
+    cin>>size;
+
+    vector<int>v(size);
+    cout<<"Enter vector Elements: ";
+    for(int i = 0;i < size;i++){
+        cin>>v[i];
     }
 
-    // Test Case 2: Sorted aur Rotated nahi hai (False aana chahiye)
-    vector<int> v2 = {2, 1, 3, 4}; 
-    if (checkSortedRotated(v2)) {
-        cout << "v2: True" << endl;
-    } else {
-        cout << "v2: False (NOT Sorted & Rotated)" << endl;
-    }
-
-    return 0;
+    bool res = checkSortedRotated(v);
+    cout<<res;
 }
