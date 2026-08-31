f457b889527a336f70a0ae715e38c9054dddc9b3
Kshitij Verma <kshitijverma577@gmail.com>
2026-08-31 21:44:34 +0530

    feat: add leetcode question #53 maxSubArray

---
+++ leetcode/02-maximumSubArray.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+// 53. Maximum Subarray
+
+// Given an integer array nums, find the subarray with the largest sum, and return its sum. 
+
+// Example 1:
+
+// Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+// Output: 6
+// Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+// Example 2:
+
+// Input: nums = [1]
+// Output: 1
+// Explanation: The subarray [1] has the largest sum 1.
+// Example 3:
+
+// Input: nums = [5,4,-1,7,8]
+// Output: 23
+// Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+int maxSubArray(vector<int> nums){
+
+    int currentSum = nums[0];
+    int maxSum = nums[0];
+
+    for(int i=0;i<nums.size();i++){
+
+        if(currentSum < 0)
+            currentSum = 0;
+        currentSum += nums[i];
+
+        if(maxSum < currentSum)
+            maxSum = currentSum;
+    }
+    return maxSum;
+}
+
+
+int main(){
+
+    int size;
+    cout<<"Enter vector size: ";
+    cin>>size;
+
+    vector<int>v(size);
+    cout<<"Enter vector elements: ";
+
+    for(int i=0;i<v.size();i++){
+        cin>>v[i];
+    }
+
+    int res = maxSubArray(v);
+    cout<<res;
+
+}
