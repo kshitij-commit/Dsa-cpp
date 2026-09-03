3ec9212e1181ab7ddf2d55c4fbd4856dd52f0a33
Kshitij Verma <kshitijverma577@gmail.com>
2026-09-03 18:38:36 +0530

    feat: insert elements at beginning in lnked list.

---
+++ linkedList/tutorial/05-insertionAtBeginning.cpp
@@ -0,0 +1,87 @@
+#include<iostream>
+using namespace std;
+
+
+
+struct sll{
+    int d;
+    char ch;
+    sll *p;
+};
+struct sll *head = NULL;
+
+// void createNode(sll*& head){
+//     sll *newNode;
+//     newNode = (struct sll*) malloc(sizeof(struct sll));
+//     cout<<"Enter data for node: ";
+//     cin>>newNode->ch>>newNode->d;
+
+//     if(head == NULL){
+//         head = newNode;
+//         newNode->p = NULL;
+//         return;
+//     }else{
+//         sll *temp = head;
+
+//         while(temp->p != NULL){
+//             temp = temp->p;
+//         }
+//         temp->p = newNode;
+//         newNode->p = NULL;
+//     }
+
+
+// }
+void display(sll*& head){
+
+    sll* temp = head;
+    if(head != NULL){
+        
+            while(temp != NULL){
+                cout<<temp->ch<<" "<<temp->d<<endl;
+                temp = temp->p;
+            }
+            
+        
+        
+
+    }else{
+        cout<<"Empty linked list: ";
+        return;
+    }
+    
+}
+void insertAtBeginning(sll*& head){
+    
+    sll *newNode;
+    newNode =  (struct sll*)malloc(sizeof(struct sll));
+    cout<<"Enter data for Node: ";
+    cin>>newNode->ch>>newNode->d;
+    if(head == NULL){
+        newNode->p = NULL;
+        head = newNode;
+    }else{
+        
+        newNode->p = head;
+        head = newNode;
+
+    }
+    return;
+
+    
+}
+
+
+int main(){
+
+    // char c;
+    // do{
+    //     cout<<"Do you want to add more nodes?Y/N: ";
+    //     cin>>c;
+    //     createNode(head);
+    // }while(c == 'Y');
+    insertAtBeginning(head);
+    display(head);
+    // insertAtMiddle(head,25,'g',3);
+
+}
